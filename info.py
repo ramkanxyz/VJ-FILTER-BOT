03b97ad253816c978831dd93598c37bb9e7bff0b
ramkanxyz <ramkamxyz@gmail.com>
2024-06-01 15:36:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('mongodb+srv://swerahu724855:<crazy&xyz>@ram.hvospfq.mongodb.net/?retryWrites=true&w=majority&appName=RAM")
+DATABASE_URI = environ.get('mongodb+srv://swerahu724855:crazy&xyz@ram.hvospfq.mongodb.net/?retryWrites=true&w=majority&appName=RAM")
 DATABASE_NAME = environ.get('DATABASE_NAME', "RAM")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'telegram')
 
